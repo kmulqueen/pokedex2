fbe89c2c7315dbcfe7d8ca2b9f86339b51561cc3
Kyle Mulqueen <kmulqdrums710@gmail.com>
2021-01-14 10:54:23 -0700

    Changed BrowserRouter to HashRouter to deploy to GitHub Pages.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import HomePage from "./pages/HomePage";
 import DetailsPage from "./pages/DetailsPage";
