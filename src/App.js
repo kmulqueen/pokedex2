231e1c9d5fbb3f2fe64b476a49ff3a1e3841cf4a
Kyle Mulqueen <kmulqdrums710@gmail.com>
2021-01-13 22:09:08 -0700

    trying to fix height collapse issue in deployed app.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-// import Container from "react-bootstrap/Container";
+import Container from "react-bootstrap/Container";
 import HomePage from "./pages/HomePage";
 import DetailsPage from "./pages/DetailsPage";
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        {/* <Container> */}
-        <Route exact path="/" component={HomePage} />
-        <Route exact path="/pokemon/:id" component={DetailsPage} />
-        {/* </Container> */}
-      </Switch>
+    <Router basename="/">
+      <Container>
+        <Switch>
+          <Route exact path="/pokemon/:id" component={DetailsPage} />
+          <Route exact path="/" component={HomePage} />
+        </Switch>
+      </Container>
     </Router>
   );
 }
