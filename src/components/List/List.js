33b5b0d2c27dd36072b66ff9681a9c9ec2d3762c
Kyle Mulqueen <kmulqdrums710@gmail.com>
2021-01-13 22:29:08 -0700

    Moved comment.

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -3,36 +3,34 @@ import Table from "react-bootstrap/Table";
 import ListItem from "../ListItem";
 
 const List = ({ pokemonList }) => {
-  return (
+  return pokemonList.length ? (
     // If the pokemonList has content, render the results in the list as a table
-    pokemonList.length ? (
-      <Table striped bordered hover responsive>
-        <thead>
-          <tr>
-            <th>No.</th>
-            <th>Name</th>
-            <th>Type</th>
-            <th>Weaknesses</th>
-            <th>Details</th>
-          </tr>
-        </thead>
-        <tbody>
-          {/* Map through the pokemonList and render a list item for each pokemon */}
-          {pokemonList.map((pokemon, idx) => (
-            <ListItem
-              key={pokemon.id}
-              number={pokemon.num}
-              name={pokemon.name}
-              type={pokemon.type}
-              weaknesses={pokemon.weaknesses}
-            />
-          ))}
-        </tbody>
-      </Table>
-    ) : (
-      // If pokemonList is an empty array, return message
-      <p>No Pokemon Found.</p>
-    )
+    <Table striped bordered hover responsive>
+      <thead>
+        <tr>
+          <th>No.</th>
+          <th>Name</th>
+          <th>Type</th>
+          <th>Weaknesses</th>
+          <th>Details</th>
+        </tr>
+      </thead>
+      <tbody>
+        {/* Map through the pokemonList and render a list item for each pokemon */}
+        {pokemonList.map((pokemon, idx) => (
+          <ListItem
+            key={pokemon.id}
+            number={pokemon.num}
+            name={pokemon.name}
+            type={pokemon.type}
+            weaknesses={pokemon.weaknesses}
+          />
+        ))}
+      </tbody>
+    </Table>
+  ) : (
+    // If pokemonList is an empty array, return message
+    <p>No Pokemon Found.</p>
   );
 };
 
